970d60f5932a944121b882e629f9171aaae0a3ec
flamingflarion <jonlogidagbjartsson@gmail.com>
2025-03-19 17:58:21 +0000

    removed debug print

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -5,11 +5,10 @@ const SPEED = 100.0
 var _health: int = 100
 
 func _physics_process(delta: float) -> void:
-	# store direction as vector with no y coordinate for consistent x movement
+	# store direction as vector with no y coordinate for consistant x movement
 	var dirVector = Vector2((Game.get_player().global_position.x-global_position.x),0)
 	var dir = dirVector.normalized()
 	velocity.x = dir.x * SPEED
-	print("velocity: ",dir)
 	move_and_slide()
 
 func take_damage(amount: int) -> void:
