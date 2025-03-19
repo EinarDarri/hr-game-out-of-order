fc3781978e04cc9d02dd4a19c36458e09bf6d1b3
flamingflarion <jonlogidagbjartsson@gmail.com>
2025-03-19 17:50:22 +0000

    Fixed enemy movement speed

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -5,8 +5,11 @@ const SPEED = 100.0
 var _health: int = 100
 
 func _physics_process(delta: float) -> void:
-	var dir = (Game.get_player().global_position - global_position).normalized()
+	# store direction as vector with no y coordinate for consistent x movement
+	var dirVector = Vector2((Game.get_player().global_position.x-global_position.x),0)
+	var dir = dirVector.normalized()
 	velocity.x = dir.x * SPEED
+	print("velocity: ",dir)
 	move_and_slide()
 
 func take_damage(amount: int) -> void:
