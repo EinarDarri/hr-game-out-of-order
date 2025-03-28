bd69914d931b3e239959d37b62ee6137be5cb7cf
flamingflarion <jonlogidagbjartsson@gmail.com>
2025-03-28 09:04:46 +0000

    fixed timer time between and added spawn cap

--- spawner/spawner.gd
+++ spawner/spawner.gd
@@ -6,6 +6,8 @@ class_name Spawner extends Node2D
 
 @export var spawing: PackedScene
 @export var time_between_spawns: float = 5.0
+@export var total_spawns: int = 3
+var count_spawns: int = 0
 
 @export_group("Spawn box")
 @export var LIMIT_LEFT:int = -250:
@@ -29,6 +31,9 @@ class_name Spawner extends Node2D
 func _ready() -> void:
 	if drawing == null:
 		_make_drawing_object()
+	# set timer wait time variable to same as timer inspector variable
+	timer.wait_time = time_between_spawns 
+	timer.start()
 
 func _make_drawing_object() -> void:
 	drawing = SpawnerDrawer.new()
@@ -42,15 +47,18 @@ func get_random_cords() -> Vector2:
 	var y = randf_range(LIMIT_TOP, LIMIT_BOTTOM)
 	return Vector2(x,y)
 	
-func _spawn() -> void:
-	var pos:Vector2 = get_random_cords()
-	var spawnd:= spawing.instantiate()
+func _spawn() -> void: # need to use global position as offset otherwise all spawners spawn around
+	if count_spawns >= total_spawns:
+		timer.stop()
+	else:
+		var pos:Vector2 = get_random_cords() + global_position
+		var spawnd:= spawing.instantiate()
 	
-	spawnd.position = pos
+		spawnd.position = pos
+		count_spawns+=1
 	
-	
-	$"..".add_child(spawnd)
-	timer.start()
+		$"..".add_child(spawnd)
+		timer.start()
 	
 func _draw() -> void:
 	if Engine.is_editor_hint():
