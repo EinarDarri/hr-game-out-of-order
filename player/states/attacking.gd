4375a5cc256ca5da6c38324b9dd8d24a65f12f86
einardarri <git@einardarri.is>
2025-04-01 19:01:09 +0000

    yeet over

--- player/states/attacking.gd
+++ player/states/attacking.gd
@@ -1,1 +1,13 @@
-extends Node
+extends PlayerState
+
+var perf_state: PlayerState
+var player_velocity: Vector2 
+
+func start_state() -> void:
+	player_velocity = player.velocity
+
+func update_state(delta: float) -> void:
+	pass
+	
+func end_state() -> void:
+	player.velocity = player_velocity
