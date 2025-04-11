a5665cf425eb3bfa361fc25d06cff754ccd3e50d
flamingflarion <jonlogidagbjartsson@gmail.com>
2025-04-11 20:06:17 +0000

    temp particle affect added to sword enemy

--- enemy/enemies/Spirit_blade/damage_effects.gd
+++ enemy/enemies/Spirit_blade/damage_effects.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@export var take_damage_particle: PackedScene
 @onready var spirit_blade: Enemy = $".."
 
 # Called when the node enters the scene tree for the first time.
@@ -15,3 +16,7 @@ func _process(delta: float) -> void:
 func _on_spirit_blade_attack_received(attack: Attack) -> void:
 	if spirit_blade.get_health() == 0:
 		pass
+	else:
+		var strike := take_damage_particle.instantiate()
+		add_child(strike)
+		strike.emitting = true # manually turn on emitting since blood is a oneshot.
