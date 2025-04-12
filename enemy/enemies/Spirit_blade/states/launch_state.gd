b77d8d2ec8dcd20c62fe0c6fbb9edddb91be2d5f
Axel Snaer <hab123321123@gmail.com>
2025-04-12 00:36:10 +0000

    Fix bug

--- enemy/enemies/Spirit_blade/states/launch_state.gd
+++ enemy/enemies/Spirit_blade/states/launch_state.gd
@@ -6,11 +6,17 @@ extends EnemyState
 @export_group("Other States")
 @export var chase_state: EnemyState
 @export var deflect_state: EnemyState
+@export var reaggro_time: float = 2.0
 
 @export var targeting_crosshair: Sprite2D
 
+@onready var reaggro_delay_timer: Timer = $ReaggroDelayTimer
+
 var _dir: Vector2
 var _start_pos: Vector2
+
+func _ready() -> void:
+	reaggro_delay_timer.timeout.connect(func(): state_manager.active_state = chase_state)
 
 func start_state() -> void:
 	_dir = enemy.global_position.direction_to(targeting_crosshair.global_position)
@@ -18,6 +24,7 @@ func start_state() -> void:
 	var rot = atan2(_dir.y, _dir.x) + PI/2
 	enemy.global_rotation = rot
 	enemy.velocity += _dir * launch_speed
+	reaggro_delay_timer.start(reaggro_time)
 
 func physics_update(delta: float) -> void:
 	enemy.velocity = Vector2(
@@ -32,3 +39,5 @@ func end_state() -> void:
 
 func attack_received(attack: Attack) -> void:
 	state_manager.active_state = deflect_state
+	reaggro_delay_timer.stop()
+	
